aebb725c3893aa9fc74405b54c2e5a1413c3124f
alectramell <alectramell@gmail.com>
2016-06-09 19:50:08 -0700

    coldsky

--- menu.sh
+++ menu.sh
@@ -7,6 +7,37 @@ WHEREAMI=$(pwd)
 
 clear
 
-zenity --title="ColdSky v1.0" --text-info --html --width="700" --height="700" --url="http"
+TIMELINE=$(date +%M)
+timeline="$TIMELINE"
 
 clear
+
+main_view() {
+
+zenity --title="ColdSky v1.0" --text-info --html --width="347" --height="389" --url="https://github.com/alectramell/coldsky/raw/master/001.gif" --ok-label="[?]" --cancel-label="EXIT"
+
+}
+
+storm_view() {
+
+zenity --title="ColdSky v1.0" --text-info --html --width="347" --height="389" --url="https://github.com/alectramell/coldsky/raw/master/002.gif" --ok-label="[?]" --cancel-label="EXIT"
+
+}
+
+clear
+
+if [ $timeline -gt "45" ]
+then
+	storm_view
+elif [ $timeline -lt "10" ]
+then
+	storm_view
+else
+	main_view
+fi
+
+clear
+
+
+
+
